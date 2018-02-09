f896e02ba0376c8a59583569036caaedf39b45ae
Nathan Wildenberg <nathan97wildenberg@gmail.com>
2018-02-09 15:00:32 +0000

    Fixed Database interface errors

--- src/com/windhound/server/DBManager.java
+++ src/com/windhound/server/DBManager.java
@@ -1,19 +1,19 @@
 package com.windhound.server;
 
-//import com.windhound.server.race.*;
+import com.windhound.server.race.*;
 
 import com.windhound.server.race.*;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 public class DBManager
 {
-    public static ArrayList<Long> getSubordinates(Long a_id)
+    public static HashSet<Long> getSubordinates(Long a_id)
     {
         Random random = new Random();
         int count     = random.nextInt(5);
-        ArrayList<Long> subordinates = new ArrayList<>();
+        HashSet<Long> subordinates = new HashSet<>();
 
         for (int i = 1; i <= count; ++i)
             subordinates.add(new Long(i));
@@ -21,11 +21,11 @@ public class DBManager
         return subordinates;
     }
 
-    public static ArrayList<Long> getManagers(Long a_id)
+    public static HashSet<Long> getManagers(Long a_id)
     {
         Random random = new Random();
         int count     = random.nextInt(5);
-        ArrayList<Long> managers = new ArrayList<>();
+        HashSet<Long> managers = new HashSet<>();
 
         for (int i = 1; i <= count; ++i)
             managers.add(new Long(i));
@@ -35,45 +35,55 @@ public class DBManager
 
     public static Championship loadChampionship(Long a_id)
     {
-        Long id                   = a_id;
-        String name               = "race_id" + id.toString();
-        Championship championship = Championship.createChampionship(id, name);
+        Long          id           = a_id;
+        String        name         = "race_id" + id.toString();
+        HashSet<Long> subordinates = getSubordinates(id);
+        HashSet<Long> managers     = new HashSet<>();//getManagers(id);
+        Championship championship  = Championship.createChampionship(id, name, subordinates, managers);
 
         return championship;
     }
 
     public static Event loadEvent(Long a_id)
     {
-        Long id     = a_id;
-        String name = "race_id" + id.toString();
-        Event event = Event.createEvent(id, name);
+        Long          id           = a_id;
+        String        name         = "race_id" + id.toString();
+        HashSet<Long> subordinates = getSubordinates(id);
+        HashSet<Long> managers     = getManagers(id);
+        Event event  = Event.createEvent(id, name, subordinates, managers);
 
         return event;
     }
     
     public static Race loadRace(Long a_id)
     {
-        Long id     = a_id;
-        String name = "race_id" + id.toString();
-        Race race   = Race.createRace(id, name);
+        Long          id           = a_id;
+        String        name         = "race_id" + id.toString();
+        HashSet<Long> subordinates = getSubordinates(id);
+        HashSet<Long> managers     = getManagers(id);
+        Race race  = Race.createRace(id, name, subordinates, managers);
 
         return race;
     }
 
     public static Boat loadBoat(Long a_id)
     {
-        Long id     = a_id;
-        String name = "boat_id" + id.toString();
-        Boat boat   = Boat.createBoat(id, name);
+        Long          id           = a_id;
+        String        name         = "race_id" + id.toString();
+        HashSet<Long> subordinates = getSubordinates(id);
+        HashSet<Long> managers     = getManagers(id);
+        Boat boat  = Boat.createBoat(id, name, subordinates, managers);
 
         return boat;
     }
 
     public static Competitor loadCompetitor(Long a_id)
     {
-        Long id               = a_id;
-        String name           = "competitor_id" + id.toString();
-        Competitor competitor = Competitor.createCompetitor(id, name);
+        Long          id           = a_id;
+        String        name         = "race_id" + id.toString();
+        HashSet<Long> subordinates = new HashSet<>(); //getSubordinates(id);
+        HashSet<Long> managers     = getManagers(id);
+        Competitor competitor  = Competitor.createCompetitor(id, name, subordinates, managers);
 
         return competitor;
     }
